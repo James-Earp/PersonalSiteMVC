320746de2aef21e422232e02082812170eb7e8cf
James-Earp <jamesaearp@outlook.com>
2020-12-14 10:28:05 -0600

    Initial Commit

---
+++ PersonalSiteMVC.UI.MVC/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using PersonalSiteMVC.UI.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonalSiteMVC.UI.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult ContactAjax(ContactViewModel cvm)
+        {
+            string body = $"{cvm.Name} has sent you the following message: <br />" + $"{cvm.Message} <strong>from the email address:</strong> {cvm.Email}";
+            MailMessage m = new MailMessage("admin@jamesearp.com", "Jamesaearp@outlook.com", cvm.Subject, body);
+            m.IsBodyHtml = true;
+            m.Priority = MailPriority.High;
+            m.ReplyToList.Add(cvm.Email);
+            SmtpClient client = new SmtpClient("mail.jamesearp.com");
+            client.Credentials = new NetworkCredential("admin@jamesearp.com", "Incorrect1999#");
+            client.Port = 8889;
+            try
+            { 
+                client.Send(m);
+            }
+            catch (Exception e)
+            {
+
+                ViewBag.Message = e.StackTrace;
+            }
+            return Json(cvm);
+        }
+        public ActionResult Resume()
+        {
+            return View();
+        }
+    }
+}
---
+++ PersonalSiteMVC.UI.MVC/Models/ContactViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PersonalSiteMVC.UI.MVC.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "* Name is required")]
+        public string Name { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "* Email is required")]
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        [Required(ErrorMessage = "Message is required")]
+        [UIHint("MultilineText")]
+        public string Message { get; set; } 
+    }
+}
